d567d462283741dfadfebe77d6c9beb0ac596fb9
cjmet <142361234+cjmet@users.noreply.github.com>
2024-02-19 17:33:35 -0500

    sql file for class

---
+++ file.sql
@@ -0,0 +1,11 @@
+SELECT CustomerID FROM Orders
+WHERE OrderID = 10310;
+
+SELECT CustomerName FROM Customers
+WHERE CustomerID = 77;
+
+SELECT SupplierID FROM Products
+Where ProductID = 40;
+
+SELECT SupplierName,Address,City,Country,PostalCode FROM Suppliers
+WHERE SupplierID = 19;
